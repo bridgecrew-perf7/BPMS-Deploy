13167abdba1aded6ede4ae9346831c3e2ae5ce7d
Duc Anh <athan@bates.edu>
2021-08-30 11:08:42 -0400

    first commit

--- src/graphql/typesDef/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.typeDefs = void 0;
-const NormalTypes_1 = require("./NormalTypes");
-const EnumTypes_1 = require("./EnumTypes");
-exports.typeDefs = [NormalTypes_1.NormalTypes, EnumTypes_1.EnumTypes];
